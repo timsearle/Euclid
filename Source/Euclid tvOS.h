f737b072f42093e70c351561c61e5e4d958b1c29
Tim Searle <timwebuk@gmail.com>
2017-04-10 10:51:12 +0100

    Refactored and re-organised the project to more cleanly support build schemes and unit testing. Full support for building with macOS, tvOS and watchOS added. Full unit test coverage added for macOS and tvOS
    gitignore file added and incorrectly tracked files removed from version control.
    
    Updated build version to 1.2.0

---
+++ Source/Euclid tvOS.h
@@ -0,0 +1,19 @@
+//
+//  Euclid tvOS.h
+//  Euclid tvOS
+//
+//  Created by Tim Searle on 10/04/2017.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Euclid tvOS.
+FOUNDATION_EXPORT double Euclid_tvOSVersionNumber;
+
+//! Project version string for Euclid tvOS.
+FOUNDATION_EXPORT const unsigned char Euclid_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Euclid_tvOS/PublicHeader.h>
+
+
