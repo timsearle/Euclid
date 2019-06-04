aa16a07d45a5893d349d40f00e75cfdb96cbc901
Tim Searle <tim.searle@karmarama.com>
2019-06-03 21:28:47 -0700

    Updating Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,20 @@
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
-    name: "Euclid"
+    name: "Euclid",
+    products: [
+        .library(
+            name: "Euclid",
+            targets: ["Euclid"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "Euclid",
+            dependencies: []),
+        .testTarget(
+            name: "EuclidTests",
+            dependencies: ["Euclid"]),
+    ]
 )
